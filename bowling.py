9f78d96b31ba331ef8262d38c6ec0ea6b53a9bba
Sandor David Olasz <olaszsandordavid@gmail.com>
2017-07-29 15:57:42 +0200

    Bowling refactored

--- bowling.py
+++ bowling.py
@@ -1,3 +1,6 @@
+numbers = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+
 def score(game):
     result = 0
     frame = 1
@@ -7,21 +10,12 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
+        if frame < 10 and get_value(game[i]) == 10:
+            result = tens_handler(game, result, i)
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
@@ -30,16 +24,23 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in numbers:
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
     else:
         raise ValueError()
+
+
+def tens_handler(game, result, i):
+    result += get_value(game[i+1])
+    if game[i] == 'x' or game[i] == 'X':
+        if game[i+2] == '/':
+            result += 10 - get_value(game[i+1])
+        else:
+            result += get_value(game[i+2])
+    return result
